da97ac99dddd1e8d3b101941c10ed9eabe4fdc23
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-17 10:11:07 +0200

    add user button to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
+import Hero from '@/components/Hero'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <main className="">
-     
+      <section className='float-right p-5'>
+        <UserButton afterSignOutUrl='/' />
+      </section>
+
+      <div className='flex justify-center items-center w-full h-screen'>
+        <Hero />
+      </div>
     </main>
   )
 }
