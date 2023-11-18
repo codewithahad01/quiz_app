fc6549f89180306f817b9f6d9f4a3f024de1dcb4
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-18 12:27:37 +0200

    add back button

--- src/app/level/page.tsx
+++ src/app/level/page.tsx
@@ -1,12 +1,22 @@
 import Button from '@/components/Button'
+import Link from 'next/link'
 import React from 'react'
+import { ArrowLeft } from 'iconsax-react'
 
 const page = () => {
   return (
-    <section className='flex justify-center items-center h-screen gap-4'>
+    <section className=''>
+      <div className='float-left  text-white p-3'>
+        <Link href='/' className='flex flex-row button gap-2'>
+          <ArrowLeft className='transform transition-transform hover:-translate-x-3' size={24} color='#fff' />
+          Back
+        </Link>
+      </div>
+      <div className='flex justify-center items-center h-screen gap-4'>
         <Button title='Easy' link='/level/easy' />
         <Button title='Medium' link='/level/medium' />
         <Button title='Hard' link='/level/hard' />
+      </div>
     </section>
   )
 }
