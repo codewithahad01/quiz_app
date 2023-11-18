fe890e89cc568ac0b6bd1c840e5d36b819fce9f5
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-18 12:28:28 +0200

    add icon to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Button'
 import Button from './Button'
+import { ArrowRight } from 'iconsax-react'
 
 const Hero = () => {
   return (
     <div className='flex flex-col'>
         <h1>Let's start the quiz!</h1>
-        <div className='flex justify-center'>
-          <Button title="Start" link='/level' />
+        <div className='flex justify-center gap-3'>
+          <Button title="Start" link='/level' icon={<ArrowRight size={24} color='#fff' />} />
         </div>
     </div>
   )
