5dca68f0926bb3eb3e44447107ae8939d00a8e32
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-18 12:51:41 +0200

    add data file

---
+++ data/ques.js
@@ -0,0 +1,96 @@
+// generate software development questions for the quiz game 
+{
+    "questions" [
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        },
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        },
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        },
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        },
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        },
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        },
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        },
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        },
+        {
+            "question": "Which of the following is not a programming language?",
+            "options": [
+                "Java",
+                "Python",
+                "C++",
+                "HTML"
+            ],
+            "answer": "HTML"
+        }
+    ]
+
+}
